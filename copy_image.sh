4e5c60aaddbfef08bec86df44740d2e44feace5b
Simpson Eng <simpsoneng552@gmail.com>
2026-03-20 03:07:23 +0800

    try beta = 1.1

--- copy_image.sh
+++ copy_image.sh
@@ -3,6 +3,6 @@ rsync -av --include='*/' --include='*.png' --exclude='*' \
     simpsone@xlogin2:~/blend2/router_output/ ./images/temp/
 # # rsync -av --include='*/' --include='*.png' --exclude='*' \
 # #  simpsone@xlogin2:~/blend2/router_hybrid_output/ ./images/temp/
-scp -r simpsone@xlogin2:~/blend2/router_hybrid_output/* ./images/temp/
+# scp -r simpsone@xlogin2:~/blend2/router_hybrid_output/* ./images/temp/
 scp -r simpsone@xlogin2:~/blend2/threshold_plots/* ./images/temp/
 scp -r simpsone@xlogin2:~/blend2/metrics_output/* ./images/temp/
